4507155a31f5de9e00f32ca6586b103458858d10
iAugus <iAugux@gmail.com>
2024-02-26 11:33:59 +0800

    update

--- Sources/ShareApps/ShareApp.swift
+++ Sources/ShareApps/ShareApp.swift
@@ -16,91 +16,98 @@ public enum ShareApp: CaseIterable, Identifiable {
     case extractor
     case xQR
     case xReload
+    case xPassword
 
     public var icon: Image {
         switch self {
         case .xsearch:
-            return .init("xsearch", bundle: .module)
+                .init("xsearch", bundle: .module)
         case .valens:
-            return .init("valens", bundle: .module)
+                .init("valens", bundle: .module)
         case .extractor:
-            return .init("xextractor", bundle: .module)
+                .init("xextractor", bundle: .module)
         case .xQR:
-            return .init("xqr", bundle: .module)
+                .init("xqr", bundle: .module)
         case .xReload:
-            return .init("xreload", bundle: .module)
+                .init("xreload", bundle: .module)
         case .player:
-            return .init("player4watch", bundle: .module)
+                .init("player4watch", bundle: .module)
         case .purge:
-            return .init("purge4safari", bundle: .module)
+                .init("purge4safari", bundle: .module)
         case .browser:
-            return .init("browser4watch", bundle: .module)
+                .init("browser4watch", bundle: .module)
         case .reset:
-            return .init("reset", bundle: .module)
+                .init("reset", bundle: .module)
         case .navify:
-            return .init("navify", bundle: .module)
+                .init("navify", bundle: .module)
         case .pix:
-            return .init("pix", bundle: .module)
+                .init("pix", bundle: .module)
         case .reader:
-            return .init("reader4watch", bundle: .module)
+                .init("reader4watch", bundle: .module)
+        case .xPassword:
+                .init("xpassword", bundle: .module)
         }
     }
 
     public var name: String {
         switch self {
         case .xsearch:
-            return "xSearch for Safari"
+            "xSearch for Safari"
         case .valens:
-            return "Valens - Widgets for Health"
+            "Valens - Widgets for Health"
         case .extractor:
-            return "Extractor for Safari"
+            "Extractor for Safari"
         case .xQR:
-            return "xQR for Safari"
+            "xQR for Safari"
         case .xReload:
-            return "xReload for Safari"
+            "xReload for Safari"
         case .player:
-            return "Player for Watch"
+            "Player for Watch"
         case .purge:
-            return "Purge for Safari"
+            "Purge for Safari"
         case .browser:
-            return "Browser for Watch"
+            "Browser for Watch"
         case .reset:
-            return "Reset - Track with Widgets"
+            "Reset - Track with Widgets"
         case .navify:
-            return "Navify - Navigate to Photo"
+            "Navify - Navigate to Photo"
         case .pix:
-            return "Pix - Photos for Watch"
+            "Pix - Photos for Watch"
         case .reader:
-            return "Reader for Watch"
+            "Reader for Watch"
+        case .xPassword:
+            "xPassword fror Safari"
         }
     }
 
     public var id: String {
         switch self {
         case .xsearch:
-            return "1579902068"
+            "1579902068"
         case .valens:
-            return "1582356757"
+            "1582356757"
         case .extractor:
-            return "1626704817"
+            "1626704817"
         case .xQR:
-            return "1602650129"
+            "1602650129"
         case .xReload:
-            return "1588834516"
+            "1588834516"
         case .player:
-            return "6446242162"
+            "6446242162"
         case .purge:
-            return "1619725381"
+            "1619725381"
         case .browser:
-            return "6445950258"
+            "6445950258"
         case .reset:
-            return "6463799353"
+            "6463799353"
         case .navify:
-            return "6474376506"
+            "6474376506"
         case .pix:
-            return "6475564605"
+            "6475564605"
         case .reader:
-            return "6446140905"
+            "6446140905"
+        case .xPassword:
+            "1621026622"
         }
     }
 }
